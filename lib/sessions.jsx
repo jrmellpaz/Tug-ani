8adca3e89bb743339cbb93c996190325ff1e3e45
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2024-10-30 09:55:24 +0800

    Implemented admin authorization.

--- lib/sessions.jsx
+++ lib/sessions.jsx
@@ -6,7 +6,7 @@ const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
 
 export async function createSession(userId) {
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);       // 7 days
   const session = await encrypt({ userId, expiresAt });
   const cookieStore = await cookies();
   cookieStore.set("session", session, {
