c32c3cb16b09c74dff83e799af8bbdd06952e0cb
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2024-10-31 14:02:35 +0800

    Implemented button states in the header.

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -17,11 +17,14 @@ export default function Header() {
         setSearchbarOpened(!searchbarOpened);
     }
 
+    const inactive = "outline-none w-12 h-12 rounded-full flex justify-center items-center hover:bg-gray-100";
+    const active = "outline-none w-12 h-12 rounded-full flex justify-center items-center bg-[#7B1113]";
+
     return (
         <nav className="fixed w-dvw">
             <div className="w-full h-16 bg-white shadow-md flex items-center justify-between pl-3 pr-3">
-                <button onClick={handleMenuClick} className="header-button" title="Menu"> 
-                    <img src="/menu.svg" alt="Menu" />
+                <button onClick={handleMenuClick} className={menuOpened ? active : inactive} title="Menu"> 
+                    <img src={menuOpened ? "/menu-active.svg" : "/menu.svg"} alt="Menu" />
                 </button>
                 <Link href={"/"}>
                     <div className="h-16 w-fit flex flex-row justify-center items-center gap-3">
@@ -29,8 +32,8 @@ export default function Header() {
                         <h1 className="font-bebas text-2xl">Tug-ani</h1>
                     </div>
                 </Link>
-                <button onClick={handleSearchbarClick} className="header-button" title="Search">
-                    <img src="/search.svg" alt="Search" />
+                <button onClick={handleSearchbarClick} className={searchbarOpened ? active : inactive} title="Search">
+                    <img src={searchbarOpened ? "/search-active.svg" : "/search.svg"} alt="Search" />
                 </button>
             </div>
             {searchbarOpened && <Searchbar />}
