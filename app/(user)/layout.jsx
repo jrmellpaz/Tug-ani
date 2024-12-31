41b730b9587fffce93a63dbeaba7099e0bebe96e
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2024-12-31 18:13:49 +0800

    Added loading progress bar at top of user-side pages.

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,9 +1,14 @@
 import Header from "@/app/components/Header/Header";
 import Menu from "@/app/components/Header/Menu";
+import NextTopLoader from "nextjs-toploader";
 
 export default function AdminLayout({ children }) {
     return (
         <>
+            <NextTopLoader 
+                color="#5a051b"
+                showSpinner={false}
+            />
             <Header>
                 <Menu />
             </Header>
