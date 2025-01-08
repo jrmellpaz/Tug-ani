9d94ed1e585644f8d2427751dff461043e99a914
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-01-08 11:45:25 +0800

    Implemented footer.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { getCategories } from "@/lib/firebase/category/read_server";
-import { Facebook, Mail, MapPin } from "lucide-react";
+import { Mail, MapPin } from "lucide-react";
 import Link from "next/link";
 
 export async function Footer() {
