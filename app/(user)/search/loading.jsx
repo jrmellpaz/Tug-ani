602571752ef1214a70b9eb897865b73e4cb372aa
kapadilla <kapadilla@up.edu.ph>
2025-05-21 20:23:45 +0800

    Added loading.jsx for faster loading

---
+++ app/(user)/search/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <div className="flex items-center justify-center h-screen">
+                <div className="loading text-tugAni-red loading-lg" />
+        </div>
+    );
+}
